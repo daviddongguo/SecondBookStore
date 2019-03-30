2c382cdc9c681a5b1e068dbed9275e8c44ab7e62
daviddongguo <david.dong.guo@gmail.com>
2019-03-30 17:35:58 -0400

    1 [new] add interface IRepository
    2 [new] add class ProductRepository

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Abstract/IProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿using David.SecondBook.OnlineStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace David.SecondBook.OnlineStore.Domain.Abstract
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Product> ProductsList { get; }
+    }
+}
