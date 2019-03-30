806fa4bc903775cb0c5a3a6193b0738b11d78d6e
daviddongguo <david.dong.guo@gmail.com>
2019-03-30 17:09:40 -0400

    [new] add console project

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Test.DebugConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace David.SecondBook.OnlineStore.Test.DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("started.");
+
+            Console.WriteLine("closing...");
+
+            Console.ReadKey();
+        }
+    }
+}
