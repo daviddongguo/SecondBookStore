fb047bb30f0d4246e3fb12f9ed9ed85b9720aaf7
daviddongguo <david.dong.guo@gmail.com>
2019-03-31 10:04:02 -0400

    [new] add product controller and view

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using David.SecondBook.OnlineStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace David.SecondBook.OnlineStore.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository rep;
+
+        public ProductController(IProductsRepository rep)
+        {
+            this.rep = rep;
+        }
+
+        //
+        public ViewResult List()
+        {
+            return View(rep.ProductsList);
+        }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
