4ff4f8c66482bf29a4af25a39912eb72526ec256
daviddongguo <david.dong.guo@gmail.com>
2019-03-31 10:02:22 -0400

    [new] add three concrete XML, EFDb, MOCK

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Concrete/XMLProductsRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace David.SecondBook.OnlineStore.Domain.Entities
+{
+    using David.SecondBook.OnlineStore.Domain.Abstract;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class XMLProductsRepository : IProductsRepository
+    {
+        public IEnumerable<Product> ProductsList => throw new NotImplementedException();
+    }
+}
---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Mock/MockProductsRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace David.SecondBook.OnlineStore.Domain.Abstract
+{
+    using David.SecondBook.OnlineStore.Domain.Entities;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MockProductsRepository : IProductsRepository
+    {
+        public IEnumerable<Product> ProductsList
+        {
+            get
+            {
+                return new List<Product>
+                {
+                    new Product { Name = "Mock Football", Price = 25 },
+                    new Product { Name = "Mock Surf board", Price = 179 },
+                    new Product { Name = "Mock Running shoes", Price = 95 }
+                };
+            }
+        }
+    }
+}
