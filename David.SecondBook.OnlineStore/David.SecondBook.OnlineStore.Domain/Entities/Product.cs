7f5b2bf09b4b305f8295c756d87f10d1365b8004
daviddongguo <david.dong.guo@gmail.com>
2019-03-31 09:30:38 -0400

    [chg] add property

--- David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Entities/Product.cs
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Entities/Product.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace David.SecondBook.OnlineStore.Domain.Entities
+﻿namespace David.SecondBook.OnlineStore.Domain.Entities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public class Product
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
-        public int MyProperty { get; set; }
+
+        public decimal Price { get; set; }
+        public string Description { get; set; }
     }
 }
