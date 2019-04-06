3a0a06500c722ad7ea92f03972e5653e7ed0c315
daviddongguo <david.dong.guo@gmail.com>
2019-04-06 17:11:31 -0400

    [new] add paging html helper

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApp/HtmlHelpers/PagingHelpers.cs
@@ -0,0 +1,36 @@
+﻿namespace David.SecondBook.OnlineStore.WebApp.HtmlHelpers
+{
+    using David.SecondBook.OnlineStore.WebApp.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(
+            this HtmlHelper html,
+            PagingInfo pagingInfo,
+            Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pagingInfo.TotalPages; i++)  // page 1 2 3....
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pagingInfo.CurrentPage)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
+
