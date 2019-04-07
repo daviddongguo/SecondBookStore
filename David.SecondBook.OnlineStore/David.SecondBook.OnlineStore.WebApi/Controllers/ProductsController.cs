2aa0e81d8232e44c3bffadc234b28f2d82d8b09a
daviddongguo <david.dong.guo@gmail.com>
2019-04-07 12:26:02 -0400

    [new] add GET method

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using David.SecondBook.OnlineStore.Domain.Abstract;
+using David.SecondBook.OnlineStore.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace David.SecondBook.OnlineStore.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductsRepository _rep = new MockProductsRepository();
+
+        public IEnumerable<Product> Get()
+        {
+            return _rep.ProductsList;
+        }
+    }
+}
