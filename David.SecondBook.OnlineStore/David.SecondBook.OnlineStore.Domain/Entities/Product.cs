4c5cbc81c00108a193bbe52e22d103c835ded1d7
daviddongguo <david.dong.guo@gmail.com>
2019-04-07 20:32:36 -0400

    [updae] add data annotation fro entity

--- David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Entities/Product.cs
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Entities/Product.cs
@@ -2,14 +2,17 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
 
     public class Product
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required, StringLength(50)]
         public string Name { get; set; }
 
         public decimal Price { get; set; }
