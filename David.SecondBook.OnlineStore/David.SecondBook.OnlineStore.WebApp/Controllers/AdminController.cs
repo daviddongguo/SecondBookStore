6d29cd97946e3573c3a63292965b5dfd8a7bc65e
daviddongguo <david.dong.guo@gmail.com>
2019-04-20 15:35:30 -0400

    [new] add admin control and it's list view automatically

---
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApp/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using David.SecondBook.OnlineStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace David.SecondBook.OnlineStore.WebApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductsRepository repository;
+        public AdminController(IProductsRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.ProductsList);
+        }
+    }
+}
