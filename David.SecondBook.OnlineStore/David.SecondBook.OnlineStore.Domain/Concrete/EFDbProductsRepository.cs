2579871297110c7794308f96f926e11a2cde839b
daviddongguo <david.dong.guo@gmail.com>
2019-04-27 11:32:53 -0400

    [chg] change layout

--- David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Concrete/EFDbProductsRepository.cs
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.Domain/Concrete/EFDbProductsRepository.cs
@@ -11,29 +11,57 @@ namespace David.SecondBook.OnlineStore.Domain.Concrete
     public class EFDbProductsRepository : IProductsRepository
     {
 
-        public EFDbContext Context = new EFDbContext();
+        public EFDbContext context = new EFDbContext();
 
         public IEnumerable<Product> ProductsList
         {
-            get { return this.Context.ProductsList; }
+            get { return this.context.ProductsList; }
         }
 
         public int Add(Product product)
         {
-            var dbProduct = this.Context.ProductsList.Add(product);
-            this.Context.SaveChanges();
+            var dbProduct = this.context.ProductsList.Add(product);
+            this.context.SaveChanges();
 
             return dbProduct.Id;
         }
 
-        public void Delete(int id)
+        public Product DeleteProduct(int id)
         {
-            throw new NotImplementedException();
+            var dbProduct = FindById(id);
+            if(dbProduct != null)
+            {
+                this.context.ProductsList.Remove(dbProduct);
+                this.context.SaveChanges();
+            }
+
+            return dbProduct;
         }
 
         public Product FindById(int id)
         {
-            throw new NotImplementedException();
+            return this.context.ProductsList.FirstOrDefault( p => p.Id == id);
+        }
+
+        public void SaveProduct(Product product)
+        {
+            if (product.Id == 0)
+            {
+                context.ProductsList.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.ProductsList.Find(product.Id);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+
         }
 
         public void Update(int id, Product Product)
--- David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApp/Controllers/AdminController.cs
+++ David.SecondBook.OnlineStore/David.SecondBook.OnlineStore.WebApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using David.SecondBook.OnlineStore.Domain.Abstract;
+using David.SecondBook.OnlineStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,58 @@ namespace David.SecondBook.OnlineStore.WebApp.Controllers
         {
             return View(repository.ProductsList);
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Product product = this.repository.ProductsList.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                this.repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // there is something wrong with the data values
+                return View(product);
+            }
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            Product product = this.repository.ProductsList.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var dbProduct = this.repository.DeleteProduct(id);
+            if (dbProduct != null)
+            {                
+                TempData["message"] = string.Format("{0} has been Deleted", dbProduct.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["message"] = string.Format("{0} has not been found", id);
+                return RedirectToAction("Edit");
+            }
+
+        }
+
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
     }
 }
